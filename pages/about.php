84e81e7c845e76c7d1491415a60843aa27aa1e0b
ToonnaEmeka <Toonnae63@gmail.com>
2024-12-05 19:09:53 -0500

    Update

---
+++ pages/about.php
@@ -0,0 +1,4 @@
+<?php include('../includes/header.php'); ?>
+<h1>About Us</h1>
+<p>This is a portfolio management tool for showcasing Web Dev Project, I hope you Enjoy.</p>
+<?php include('../includes/footer.php'); ?>
